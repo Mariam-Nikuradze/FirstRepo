6c4c6711c38fdc3ceaf42dde70d9a7071d627415
Mariam-Nikuradze <mariam.nikuradze2016@gipa.ge>
2017-11-11 19:40:11 +0400

    Update Java.js
    update Java file

--- Java.js
+++ Java.js
@@ -1,1 +1,15 @@
-
+var dateform
+speed=1000
+tid = 0;
+function dodate()
+{
+  f.date.value=new Date();
+  tid=window.setTimeout("dodate()",speed);
+}
+function start(x) {
+  f=x
+  tid=window.setTimeout("dodate()",speed);
+}
+function cleartid() {
+  window.clearTimeout(tid);
+}
